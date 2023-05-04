acb9f9c8c40b3fd2525ba38d221c1b50c5bdd5c1
rubenmarindev <rubenmarin@gmail.com>
2023-05-04 08:35:12 -0500

    Addition of token auth, current user helper, specs fixing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :update]
 
   rescue_from Exception do |e|
     render json: { error: e.message }, status: :internal_error
@@ -20,20 +21,23 @@ class PostsController < ApplicationController
   #GET /posts/id
   def show
     @post = Post.find(params[:id])
-
-    render json: @post, status: :ok
+    if (@post.published? || (Current.user) && (@post.user.id == Current.user.id))
+      render json: @post, status: :ok
+    else
+      render json: { error: 'Not found' } , status: :not_found
+    end
   end
 
   #POST /posts/
   def create
-    @post = Post.create!(create_params)
+    @post = Current.user.posts.create!(create_params)
 
     render json: @post, status: :created
   end
 
   #POST /posts/id
   def update
-    @post = Post.find(params[:id])
+    @post = Current.user.posts.find(params[:id])
     @post.update!(update_params)
 
     render json: @post, status: :ok
@@ -42,10 +46,23 @@ class PostsController < ApplicationController
   private
 
   def create_params
-    params.require(:post).permit(:title, :content, :published, :user_id)
+    params.require(:post).permit(:title, :content, :published)
   end
 
   def update_params
     params.require(:post).permit(:title, :content, :published)
+  end
+
+  def authenticate_user!
+    token_regex = /Bearer (\w+)/
+    headers = request.headers
+    if headers['Authorization'].present? && headers['Authorization'].match(token_regex)
+      token = headers['Authorization'].match(token_regex)[1]
+      if (Current.user = find_by_auth_token(token))
+        return
+      end
+    end
+
+    render json:{ error: 'Unauthorized'}, status: :unauthorized
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { minimum: 5, maximum: 50 }
   validates :name, presence: true, length: { minimum: 5, maximum: 50 }
   validates :auth_token, presence: true, length: { minimum: 5, maximum: 50 }
+
+  after_initialize :generate_auth_token
+
+  def generate_auth_token
+    unless auth_token.present?
+      self.auth_token = TokenGenerationService.generate
+    end
+  end
 end
